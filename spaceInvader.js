caf8420c796efbb2f415c9b51588b3a39af51e19
Wassim Bakkioui <wassim.bakkioui@student.ehb.be>
2022-10-09 19:07:25 +0200

    Space Invader Wassim

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,32 @@
 "use strict";
 
-alert("Draw your space invader here");
+
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawBox();
+
+function drawBox(){
+
+    // rect grey
+
+    context.fillStyle = '#F0F0F0';
+    context.beginPath();
+    context.rect(50,50,300,300);
+    context.fill();
+
+    // rect green
+    
+    context.fillStyle = '#ADD15E';
+    context.beginPath();
+    context.rect(80,80,50,50);
+    context.rect(280,80,50,50);
+    context.rect(130,130,150,100);
+    context.rect(280,230,50,100);
+    context.rect(180,230,50,100);
+    context.rect(80,230,50,100);
+    context.fill();
+}
